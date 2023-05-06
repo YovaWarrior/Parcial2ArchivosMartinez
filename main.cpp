ca473cdbb506ffbb421f56649466d5c0946d07f5
Carlos <Cmartineza14@miumg.edu.gt>
2023-05-06 10:13:24 -0600

    Primer Programa de crear archivos y que identifique si el numero pedido por el usuario es par o impar

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <windows.h>
+
+namespace fs = std::filesystem;
+
+int main() {
+    int numbArch;
+    std::cout << "¿Cuántos archivos deseas crear? ";
+    std::cin >> numbArch;
+
+    if (numbArch % 2 == 0) { // Si es par
+        fs::path par_dir = fs::path(getenv("USERPROFILE")) / "MisDocumentos" / "par";
+        fs::create_directories(par_dir); // Crear la carpeta "par" si no existe
+
+        for (int i = 1; i <= numbArch; i++) {
+            fs::path file_path = par_dir / ("documento" + std::to_string(i) + ".txt");
+            std::ofstream file(file_path);
+            file << "Este es el archivo " << i << std::endl;
+            file.close();
+        }
+
+        std::cout << "Se crearon " << numbArch << " archivos en " << par_dir << std::endl;
+    }
+    else { // Si es impar
+        fs::path impar_dir = fs::path(getenv("USERPROFILE")) / "Escritorio" / "impar";
+        fs::create_directories(impar_dir); // Crear la carpeta "impar" si no existe
+
+        for (int i = 1; i <= numbArch; i++) {
+            fs::path file_path = impar_dir / ("documento" + std::to_string(i) + ".txt");
+            std::ofstream file(file_path);
+            file << "Este es el archivo " << i << std::endl;
+            file.close();
+        }
+
+        std::cout << "Se crearon " << numbArch << " archivos en " << impar_dir << std::endl;
+    }
+
+    return 0;
+}
